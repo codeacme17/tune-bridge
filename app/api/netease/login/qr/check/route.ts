7eb0b99c149250ebe3f27ad8e9fe209fe3a47045
codeacme17 <vir_kings@qq.com>
2025-01-04 19:10:24 +0800

    feat: add Netease QR code login check route and enhance QR code handling in component

---
+++ app/api/netease/login/qr/check/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  const urlQuery = new URLSearchParams(req.url?.split("?")[1]);
+  const key = urlQuery.get("key");
+
+  console.log("[/login/qr/create] key ====>", key);
+
+  try {
+    const res = await fetch(
+      `https://apis.netstart.cn/music/login/qr/check?key=${key}`,
+      {
+        method: "GET",
+      }
+    );
+
+    if (!res.ok) throw new Error(`[netease login GET] Error: ${res.status}`);
+    const data = await res.json();
+    console.log("[/login/qr/key] data ====>", data);
+    return new NextResponse(JSON.stringify(data));
+  } catch (error) {
+    console.log("[netease login GET] Error: ", error);
+    return new NextResponse(error as string, { status: 500 });
+  }
+};
--- components/brand/netease.tsx
+++ components/brand/netease.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -17,31 +17,71 @@ import { Label } from "@/components/ui/label";
 import { NeteaseIcon } from "../icon/netease";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+type TabType = "qrCode" | "password";
+
 export const Netease = () => {
   const [isOpenDialog, setIsOpenDialog] = useState(false);
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
   const [qrImg, setQrImg] = useState("");
+  const [currentTab, setCurrentTab] = useState<TabType>("qrCode");
+
+  // store the key for qr code
+  const unikey = useRef(null);
 
   useEffect(() => {
-    if (!isOpenDialog) return;
+    if (!isOpenDialog || currentTab !== "qrCode") return;
     getQRCode();
-  }, [isOpenDialog]);
+  }, [isOpenDialog, currentTab]);
+
+  useEffect(() => {
+    if (!unikey.current) return;
+
+    const timer = setInterval(() => {
+      if ((!isOpenDialog || currentTab !== "qrCode") && timer) {
+        clearInterval(timer);
+        return;
+      }
+      queryQrStatus();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isOpenDialog, currentTab]);
 
   const getQRCode = async () => {
-    const keyRes = await fetch(`/api/netease/login/qr/key`, {
-      method: "GET",
-    });
-    const { data: keyData } = await keyRes.json();
-    const { unikey: key } = keyData || {};
+    try {
+      const keyRes = await fetch(`/api/netease/login/qr/key`, {
+        method: "GET",
+      });
+      const { data: keyData } = await keyRes.json();
+      const { unikey: key } = keyData || {};
+      unikey.current = key;
 
-    const createRes = await fetch(`/api/netease/login/qr/create?key=${key}`, {
-      method: "GET",
-    });
-    const { data: createData } = await createRes.json();
-    const { qrimg } = createData || {};
+      const createRes = await fetch(`/api/netease/login/qr/create?key=${key}`, {
+        method: "GET",
+      });
+      const { data: createData } = await createRes.json();
+      const { qrimg } = createData || {};
 
-    setQrImg(qrimg);
+      setQrImg(qrimg);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const queryQrStatus = async () => {
+    if (!unikey.current) return;
+    const res = await fetch(
+      `/api/netease/login/qr/check?key=${unikey.current}`,
+      {
+        method: "GET",
+      }
+    );
+    const { data } = await res.json();
+
+    console.log("data", data);
   };
 
   const handleSubmit = async () => {
@@ -81,14 +121,21 @@ export const Netease = () => {
             </DialogDescription>
           </DialogHeader>
 
-          <Tabs defaultValue="qrCode" className="w-[400px]">
+          <Tabs
+            defaultValue="qrCode"
+            className="w-[400px]"
+            value={currentTab}
+            onValueChange={(value) => {
+              setCurrentTab(value as TabType);
+            }}>
             <TabsList>
               <TabsTrigger value="qrCode">Scan</TabsTrigger>
               <TabsTrigger value="password">Password</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="qrCode" className="mt-2">
-              <div className="flex justify-center gap-2">
+            {/*  QR CODE */}
+            <TabsContent value="qrCode" className="mt-5">
+              <div className="flex justify-between mx-3">
                 <div>
                   <Image
                     src="https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/34905050930/6160/8991/0f0f/85d4094a013dc7c0709fd198152ee9f7.png"
@@ -103,11 +150,13 @@ export const Netease = () => {
                     width={200}
                     height={200}
                     alt="Picture of the author"
+                    className="rounded-md"
                   />
-                  {/* <QRCode value={qrCodeUrl} /> */}
                 </div>
               </div>
             </TabsContent>
+
+            {/* PASSWORD */}
             <TabsContent value="password">
               <div className="grid gap-4 py-4">
                 <div className="grid grid-cols-4 items-center gap-4">
@@ -140,7 +189,7 @@ export const Netease = () => {
 
               <DialogFooter>
                 <Button type="submit" onClick={handleSubmit}>
-                  Save changes
+                  Login
                 </Button>
               </DialogFooter>
             </TabsContent>
